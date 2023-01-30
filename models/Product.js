cc0d2e30af16c6e9af8fd4aa3d5290989e5bec92
Gabe Morris <gabemorris1042@gmail.com>
2023-01-30 19:46:38 -0400

    added content to all the Sequelize models

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+const { validate } = require('../../../UNB-VIRT-FSF-PT-10-2022-U-LOLC/13-ORM/01-Activities/22-Stu_One-to-One/Unsolved/config/connection');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,7 +10,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        is: DataTypes.DECIMAL
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        is: DataTypes.INTEGER
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
